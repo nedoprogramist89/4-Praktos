eec1ded5aad00154bcdcdd26479e65985fc07424
nedoprogramist89 <145318384+nedoprogramist89@users.noreply.github.com>
2023-12-21 10:27:19 +0300

    Create class.cs

---
+++ class.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Note
+    {
+        private string name, description;
+        private DateTime date;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+    }
+}
